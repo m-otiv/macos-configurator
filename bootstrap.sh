c84dd873de5ee060b33e0ccdbacb4ad926aec888
Vito Mannina <mannina.vito@gmail.com>
2021-12-07 00:03:07 +0100

    Add permission to run the file

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,8 @@
 # Clone the repo, install everything and delete everything
 
 git clone https://github.com/m-otiv/macos-configurator.git
+
+chmod +x macos-configurator/install.sh
 ./macos-configurator/install.sh
+
 rm -rf macos-configurator
